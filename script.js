1808bcd0650f58f91d6a4019b60bc073c2b06a92
Stasiperez85 <Stasiperez85@gmail.com>
2022-08-22 17:36:07 -0600

    updated css and add javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+})
+
+document.getElementsByClassName("article")[0].addEventListener("click", function () {
+    window.open("https://blog.codinghorror.com/")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function () {
+    window.open("https://blog.codinghorror.com/")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function () {
+    window.open("https://blog.codinghorror.com/")
+})
